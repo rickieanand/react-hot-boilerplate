a101c438d505611a1d72e6e3941a388734a51561
Rickie Anand <rickieanand@Apples-MacBook-Pro-2.local>
2016-05-21 18:16:20 +0530

    Added fooer and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-
+import Header from './Header';
+import Footer from './Footer';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+    	<div>
+    		<Header />
+      		<h1>Hello, world.</h1>
+      		<Footer />
+    	</div>
     );
   }
 }
